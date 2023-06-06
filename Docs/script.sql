cbdef4320cce2e9a199ced7ed85d4d9fba677828
apphrodit <joaovrdf@gmail.com>
2023-06-06 10:05:26 -0300

    users

---
+++ Docs/script.sql
@@ -0,0 +1,19 @@
+drop database if exists usuarios;
+
+create database usuarios;
+
+use usuarios;
+
+create table users(
+    id int not null auto_increment primary key,
+    login varchar(50) not null,
+    senha varchar(50) not null
+);
+
+insert into users VALUES
+(default, 'jair', password('maria')),
+(default, 'mariana', password('03101990')),
+(default, 'julia', password('123456')),
+(default, 'rafaela', password('rafal123'));
+
+select * from users;
